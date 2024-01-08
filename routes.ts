5a07012f91aa2dbb59706fda652394e4be2e013b
xdream oldlu <xdream@gmail.com>
2024-01-08 21:45:11 +0800

    start callbacks

---
+++ routes.ts
@@ -0,0 +1,24 @@
+/**
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+export const publicRoutes = ["/"]
+
+/**
+ * These routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+export const authRoutes = ["/auth/login", "/auth/register"]
+
+/**
+ * The prefix for API authentication routes
+ * Routes start with this is for API authentication purposes
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth"
+
+/**
+ * The default redirect path after logging in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
